cc561ed2ed3938a320e7d2bcad5693e6db430e12
Sayantan Roy <sayantanroy184@gmail.com>
2024-08-26 21:11:59 +0530

    Add files

---
+++ script.js
@@ -0,0 +1,8 @@
+const downloadBtn = document.querySelector(".downloads");
+
+const fileLink = "";
+
+const theclick = ()=>{
+    location.href=fileLink
+}
+downloadBtn.addEventListener("click", theclick);
